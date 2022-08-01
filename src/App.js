be08c46da26330b6e3dbe59452e413dbfd2360a0
Grzegorz Patyk <70432509+grzegorzxpatyk@users.noreply.github.com>
2022-08-01 12:05:00 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,110 @@
+import React, { useRef, useState } from "react";
+import { globalCss, styled } from "@stitches/react";
+import { blackA } from "@radix-ui/colors";
+import * as LabelPrimitive from "@radix-ui/react-label";
+import Button from "./Button";
+
+const globalStyles = globalCss({
+  "*": {
+    margin: 0,
+    padding: 0,
+    fontFamily: "Inter, sans-serif",
+    boxSizing: "border-box"
+  }
+});
+
+const H1 = styled("h1", {
+  fontSize: "2rem",
+  marginBottom: "1rem"
+});
+
+const StyledLabel = styled(LabelPrimitive.Root, {
+  fontSize: 15,
+  fontWeight: 500,
+  color: "white",
+  userSelect: "none"
+});
+
+const Label = StyledLabel;
+
+// Your app...
+const Flex = styled("div", { display: "flex" });
+const Input = styled("input", {
+  all: "unset",
+  width: "min(200px, 30vw)",
+  display: "inline-flex",
+  alignItems: "center",
+  justifyContent: "center",
+  borderRadius: 4,
+  padding: "0 10px",
+  height: 35,
+  fontSize: 15,
+  lineHeight: 1,
+  color: "white",
+  backgroundColor: blackA.blackA5,
+  boxShadow: `0 0 0 1px ${blackA.blackA9}`,
+  "&:focus": { boxShadow: `0 0 0 2px black` }
+});
+
+export default function App() {
+  globalStyles();
+
+  const [inputValue, setInputValue] = useState("Pedro Duarte");
+  const inputRef = useRef(null);
+
+  return (
+    <div
+      style={{
+        height: "100vh",
+        background: "linear-gradient(45deg, blue, red)",
+        padding: "5rem",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center"
+      }}
+    >
+      <H1>Hello{inputValue && ` ${inputValue}`}!</H1>
+      <Flex
+        css={{
+          // height: "100%",
+          padding: "0 20px",
+          flexWrap: "wrap",
+          alignItems: "center",
+          justifyContent: "center"
+        }}
+      >
+        <Label
+          htmlFor="firstName"
+          css={{ lineHeight: "35px", marginRight: 15 }}
+        >
+          First name
+        </Label>
+        <Input
+          type="text"
+          id="firstName"
+          defaultValue="Pedro Duarte"
+          ref={inputRef}
+        />
+      </Flex>
+      <Flex css={{ margin: "2rem auto" }}>
+        <Button
+          onClick={() => {
+            setInputValue(inputRef.current.value);
+          }}
+        >
+          Save
+        </Button>
+        <Button
+          type="reset"
+          onClick={() => {
+            inputRef.current.value = "";
+            setInputValue("");
+          }}
+        >
+          Reset
+        </Button>
+      </Flex>
+    </div>
+  );
+}
---
+++ src/Button.js
@@ -0,0 +1,50 @@
+import { styled } from "@stitches/react";
+
+const Button = styled("button", {
+  all: "unset",
+  padding: ".8rem 1.2rem",
+  margin: ".4rem",
+  borderRadius: "1.6rem",
+  fontFamily: '"JetBrains Mono", monospace',
+  cursor: "pointer",
+  transition: "transform .08s ease-in-out",
+  "&:active": {
+    transform: "translate(0, 2px) scale(0.95)"
+  },
+
+  variants: {
+    variant: {
+      dark: {
+        background: "#333333",
+        color: "#e3e3e3",
+        "&:hover": {
+          background: "#444444"
+        }
+      },
+      light: {
+        background: "#e3e3e3",
+        color: "#22181c",
+        "&:hover": {
+          background: "#c6c6c6"
+        }
+      },
+      outlined: {
+        backgroundColor: "#ffffff00",
+        backdropFilter: "blur(1px)",
+        transition:
+          "transform .08s ease-in-out, backdrop-filter .15s ease-in-out, background-color 0.3s ease-in-out",
+        color: "#22181c",
+        border: "1px solid #11181c",
+        "&:hover": {
+          backdropFilter: "blur(2px)",
+          backgroundColor: "#e6e6e633"
+        }
+      }
+    }
+  },
+  defaultVariants: {
+    variant: "dark"
+  }
+});
+
+export default Button;
